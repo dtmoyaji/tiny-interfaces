0f07cfbf1e9a658404a57fcf2d695554ab5d6f42
楽曲作りまくりおじさん <60741124+dtmoyaji@users.noreply.github.com>
2022-11-18 17:44:53 +0900

    IAuthClient 追加

---
+++ src/main/java/org/tiny/IAuthClient.java
@@ -0,0 +1,23 @@
+package org.tiny;
+
+/**
+ * ユーザー認証クラスの機能定義
+ * @author DTMOyaji
+ */
+public interface IAuthClient {
+
+    /**
+     * ユーザー認証を行う
+     */
+    public boolean auth(String userid, String password);
+
+    /**
+     * ユーザー情報を取得する
+     */
+    public HashMap<String, String> getUserInfo(String userid);
+
+    public String getProperty(String key);
+
+    public void setProperty(String key, String value);
+
+}
