fa1b2d7687978bf69813d34fa6a71fd3585f01c2
murakami-date <60741124+dtmoyaji@users.noreply.github.com>
2022-11-22 11:29:26 +0900

    several change.

--- src/main/java/org/tiny/IAuthClient.java
+++ src/main/java/org/tiny/IAuthClient.java
@@ -10,14 +10,19 @@ public interface IAuthClient {
 
     /**
      * ユーザー認証を行う
+     * @param userid ユーザーID
+     * @param password パスワード
+     * @return 
      */
     public boolean auth(String userid, String password);
 
     /**
      * ユーザー情報を取得する
+     * @param userid ユーザーID
+     * @return ユーザー情報をKey,value形式で返す。
      */
     public HashMap<String, String> getUserInfo(String userid);
-
+    
     public String getProperty(String key);
 
     public void setProperty(String key, String value);
