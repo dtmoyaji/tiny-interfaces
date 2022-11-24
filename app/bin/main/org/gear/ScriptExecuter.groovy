0daa5709dc1e5dd9038b41818092652dba264527
dtmoyaji <daianji@gmail.com>
2022-11-24 17:33:34 +0900

    coment remove.

--- app/bin/main/org/gear/ScriptExecuter.groovy
+++ app/bin/main/org/gear/ScriptExecuter.groovy
@@ -1,8 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Groovy/GroovyClass.groovy to edit this template
- */
-
 package org.gear
 
 /**
